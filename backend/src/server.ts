21b66dbde5fe00e69228c275a35c68700a269f2c
SLIM212 <samuellim246@gmail.com>
2025-05-22 15:23:18 +1000

    package.json update

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -30,21 +30,21 @@ app.get("/info", (req: Request, res: Response) => {
 });
 
 // Determine the port to listen on
-const PORT = process.env.PORT || 5000; // Use the environment variable PORT, or default to 5000
+const PORT = process.env.PORT ? Number(process.env.PORT) : 5000;
 
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server running on http://localhost:${PORT}`);
+// });
 
 //////////////// for cloud deployment/////////////
 // // Determine the port to listen on
 // const PORT = process.env.PORT || 5000; // Use the environment variable PORT, or default to 5000
 
 // // Start the server
-// app.listen(PORT, '0.0.0.0', () => {
-//     console.log(`Server running on http://0.0.0.0:${PORT}`);
-// });
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
+});
 
 /**
  * Function to Login user
